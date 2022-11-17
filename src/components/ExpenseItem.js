72ea8c910f3bdf3c868bafa1d43ef5e8079f34e0
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-17 17:51:44 +0100

    first expression JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,17 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+	const expenseDate = new Date(2022, 2, 28)
+	const expenseTitle = 'Car Insurance'
+	const expenseAmount = 294.34
 	return (
 		<div className="expense-item">
-			<div> March 28th 2022 </div>
-			<div  className="expense-item__description">
-				<h2> Car Assurance</h2>
-				<div  className="expense-item__price"> $245.84</div>
+			<div> {expenseDate.toISOString()}</div>
+			<div className="expense-item__description">
+				<h2>{expenseTitle}</h2>
+				<div className="expense-item__price"> ${expenseAmount}</div>
 			</div>
-		</div>
+		</div>	
 	)
 }
 
