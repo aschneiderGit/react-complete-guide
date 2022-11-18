acfd01f0c398791179603269b7f88eafebd1749e
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-18 14:28:16 +0100

    Add Expenses year filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,29 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import {useState} from 'react';
 
 function Expenses(props) {
 	const data = props.data;
+	const [filterYear, setFilterYear] = useState('2020');
+	const submitFilterYearHandler = (submitFilterYear) => {
+		setFilterYear(submitFilterYear);
+	};
 
 	return (
-		<Card className="expenses">
-			{data.map((exp) => (
-				<ExpenseItem date={exp.date} amount={exp.amount} title={exp.title} />
-			))}
-		</Card>
+		<div>
+			<p>{filterYear}</p>
+			<ExpensesFilter
+				selected={filterYear}
+				onSubmitFilterYear={submitFilterYearHandler}
+			/>
+			<Card className="expenses">
+				{data.map((exp) => (
+					<ExpenseItem date={exp.date} amount={exp.amount} title={exp.title} />
+				))}
+			</Card>
+		</div>
 	);
 }
 
