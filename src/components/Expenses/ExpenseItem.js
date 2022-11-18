ccd3a7755147fbf9c7c6d5cd35d8c9bd239a593b
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-18 11:58:36 +0100

    manage file

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,5 @@
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
@@ -10,6 +10,7 @@ function ExpenseItem(props) {
 				<h2>{props.title}</h2>
 				<div className="expense-item__price"> ${props.amount}</div>
 			</div>
+			<button>Changle title</button>
 		</Card>
 	);
 }
